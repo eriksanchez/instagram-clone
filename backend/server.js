ed9703a5ea3ef71293a5ee0d21dbfc1b23f99d28
eriksanchez <erikcsanchezdev@gmail.com>
2021-06-24 22:01:11 -0500

    additions...

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,20 @@ app.use(express.json())
 app.use(cors());
 //DB config
 
+
+const connection_url ='mongodb+srv://admin:2UgKVuEtIFuPRdN7@cluster0.nsms6.mongodb.net/instaDB?retryWrites=true&w=majority'
+mongoose.connect(connection_url, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+mongoose.connection.once('open', () => {
+    console.log('DB Connected')
+})
 //api routes
 app.get('/', (req,res) => res.status(200).send('hello world'))
+app.post('/upload', (req,res) => {
+    
+})
 //listen
 app.listen(port, () => console.log(`listening on localhost:${port}`))
